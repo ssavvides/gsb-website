a7e868d8aebef09bab566513e3a3546682dbb51f
Savvas Savvides <savvas@purdue.edu>
2018-05-21 18:28:21 -0700

    general updates

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # create archive
-rm archives/gsb.zip
-zip -r  archives/gsb.zip . -x *.git* -x *.settings* -x .project -x .DS_Store -x LICENSE -x README.md
+#rm archives/gsb.zip
+#zip -r  archives/gsb.zip . -x *.git* -x *.settings* -x .project -x .DS_Store -x LICENSE -x README.md
 
 # upload to git repo
 git add files/
